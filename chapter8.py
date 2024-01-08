f9167b39f8d72f9133f861386dc385212825cf7b
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-08 10:36:30 +0800

    chapter 7

---
+++ chapter8.py
@@ -0,0 +1,39 @@
+# function
+# def function(username, animal_type='dog'):
+#     print(f"{username} is a {animal_type}")
+#
+#
+# function(animal_type='bird', username='ll')
+# function(username='lll', animal_type='cat')
+
+# def return_formatted_name(first_name, last_name, middle_name):
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+#
+#
+# print(return_formatted_name('yuchuan', 'liu', 'shawn'))
+
+# def return_country(first_place, second_place):
+#     ans = {}
+#     ans[f'{first_place}'] = second_place
+#     return ans
+#
+#
+# print(return_country('chongqing', 'china'))
+# def build(first, last, **user_info):
+#     user_info['first_name'] = first
+#     user_info['last_name'] = last
+#     return user_info
+#
+#
+# print(build('albert', 'einstein', location='princeton', field='physics'))
+import pizza
+pizza.make_pizza(16,'cherry')
+
+
+
+
+
