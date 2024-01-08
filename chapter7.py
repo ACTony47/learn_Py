947bc9441e452f17c0120ea387744ab10075036b
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-08 09:23:44 +0800

    chapter 7

---
+++ chapter7.py
@@ -0,0 +1,37 @@
+# user input & while loop
+# message = input("hello good morning:")
+# print(message)
+# value = input("enter a digit and i will tell you if it is :")
+# if int(value) % 2 == 0:
+#     print('good')
+# else:
+#     print("bad")
+
+# message = ""
+# while message != 'quit':
+#     message = input("enter what you thnik of")
+#     print(message)
+# active = True
+# while active:
+#     message = input('what you think of me')
+#     if message == 'quit':
+#         break
+#     else:
+#         print(message)
+
+# unconfirmed_users = ['lyc', 'jjn', 'aaa']
+# while unconfirmed_users:
+#     cur_user = unconfirmed_users.pop()
+# animals = ['cat', 'dog', 'bird', 'cat']
+# while 'cat' in animals:
+#     animals.remove('cat')
+# print(animals)
+# ans = {
+# }
+# while True:
+#     name = input("what's your name")
+#     if name == 'quit':
+#         break
+#     destination = input(f"dear {name}, where you wanna travel")
+#     ans[name] = destination
+# print(ans)
