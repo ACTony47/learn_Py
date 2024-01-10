077bfa34ed04bd4b238ae5d227349d6b51fb8a23
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-10 09:59:25 +0800

    chapter 10 first half

---
+++ chapter10.py
@@ -0,0 +1,71 @@
+# 文件 错误&异常处理 json
+# from pathlib import Path
+# path = Path('text.txt')
+# contents = path.read_text()
+# contents = contents.rstrip()
+# print(contents)
+from pathlib import Path
+#访问文件的各行
+# path = Path('text.txt')
+# contents = path.read_text().rstrip()
+# lines = contents.splitlines()
+# for line in lines:
+#     print(line[1])
+
+#big scale file
+# path = Path('text.txt')
+# contents = path.read_text()
+# lines = contents.splitlines()
+# pi = ''
+# for line in lines:
+#     pi += line.lstrip()
+#
+# name = input("enter your birthday")
+# if name in pi:
+#     print("yes")
+# else:
+#     print("no")
+# string = "hello i like your mom"
+# string = string.replace('your', 'your_dad\'s')
+# print(string)
+
+#写入文件
+# path = Path('programming.txt')
+# path.write_text('i love u')
+#
+# contents = 'i love u ,too\n'
+# contents += 'i love your mom either\n'
+# path.write_text(contents)
+
+#exception 异常
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print('you r wrong')
+# name = input('enter your file name ') #path并不会对文件进行具体操作 而是调用path的函数时才会发现文件不存在
+# path = Path(name)
+# try:
+#     contents = path.read_text(encoding='utf-8')
+# except FileNotFoundError as e:
+#     print(f"{e}")
+# else:
+#     print('file found!')
+file_list = [
+
+]
+while True:
+    file_name = input("enter file name and enter q to exit")
+    if file_name == 'q':
+        break
+    else:
+        file_list.append(file_name)
+
+for name in file_list:
+    path = Path(name)
+    try:
+        contents = path.read_text().splitlines()
+    except FileNotFoundError as e:
+        print(e)
+    else:
+        for pet in contents:
+            print(f"{pet} is a {name[0:3]}")
